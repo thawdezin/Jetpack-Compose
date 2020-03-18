a386b5b5f6c098ec592a7e87d40816b00edb9042
ThawDeZin <thawdezin@gmail.com>
2020-03-18 14:50:45 +0630

    Goodbye RecyclerView

--- app/src/main/java/com/thawdezin/violet/NestedScrollActivity.kt
+++ app/src/main/java/com/thawdezin/violet/NestedScrollActivity.kt
@@ -3,15 +3,11 @@ package com.thawdezin.violet
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.navigation.Navigator
 import androidx.ui.core.Alignment
-import androidx.ui.core.AnimationClockAmbient
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.foundation.HorizontalScroller
-import androidx.ui.foundation.ScrollerPosition
 import androidx.ui.foundation.VerticalScroller
-
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Center
 import androidx.ui.layout.Column
@@ -20,6 +16,7 @@ import androidx.ui.layout.Row
 import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Scaffold
+import androidx.ui.material.surface.Card
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 
@@ -33,30 +30,88 @@ class NestedScrollActivity : AppCompatActivity() {
 }
 
 @Preview
-@Composable fun nestedScrollContent() {
+@Composable
+fun nestedScrollContent() {
     MaterialTheme {
         Scaffold {
-            oneToVertical()
+            Container(expanded = true) {
+                oneToVertical()
+            }
         }
     }
 }
 
-
-@Preview
 @Composable
-fun oneToHorizontal(){
-    Container(alignment = Alignment.Center){
-        HorizontalScroller {
-            Row {
-                for (i in 1..100){
-                    Center{
-                        Container(width = 60.dp, height = 60.dp, alignment = Alignment.Center){ //background(Color.Transparent),
-                            Text("$i")
+fun oneToVertical() {
+    Card(color = Color.Yellow) {
+        Container(alignment = Alignment.Center, expanded = true){
+            VerticalScroller {
+                Column {
+                    for (i in 1..100) {
+                        if (i == 20) {
+                            Container(height = 300.dp) { // The height of Green
+                                aToZhorizontal()
+                            }
+                            //Text("HERE")
+                        } else {
+                            Container(height = 100.dp){ // height of Numbers
+                                Text("$i")
+                            }
                         }
                     }
-
                 }
-                Divider(color = Color.Green)
+            }
+        }
+    }
+}
+
+@Preview("AtoZ_Horizontal")
+@Composable
+fun aToZhorizontal() {
+    Card(color = Color.Green){
+        Container(alignment = Alignment.Center, expanded = true) {
+            HorizontalScroller(isScrollable = true) {
+                Row {
+                    for (i in 'a'..'z') {
+                        if (i == 'd') {
+                            Container(width = 300.dp){
+                                aToZvertical()
+                            }
+                        } else {
+                            Center(){
+                                Container(width = 100.dp){
+                                    Text(" $i ")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun aToZvertical() {
+    Card(color = Color.Red) {
+        Container(alignment = Alignment.Center, expanded = true){
+            VerticalScroller {
+                Column {
+                    for (i in 'a'..'z') {
+                        if (i=='d') {
+                            Container(height = 200.dp) {
+                                OneToHorizontal()
+                            }
+                            //Text("HERE")
+                        } else {
+                            Center(){
+                                Container(height = 100.dp){
+                                    Text("$i")
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -64,77 +119,68 @@ fun oneToHorizontal(){
 
 
 @Composable
-fun oneToVertical(){
-    Container(alignment = Alignment.Center){
-        VerticalScroller {
-            Column {
-                for (i in 1..100){
-                    if(i%20 == 0){
-                        aToZhorizontal()
-                    }else{
-                        Text("$i")
+fun OneToHorizontal(){
+    Card(color = Color.Blue){
+        Container(alignment = Alignment.Center, expanded = true) {
+            HorizontalScroller(isScrollable = true) {
+                Row {
+                    for (i in 1..100) {
+                        if (i == 10) {
+                            Container(width = 150.dp){
+                                OneVertical()
+                            }
+                        } else {
+                            Center(){
+                                Container(width = 100.dp){
+                                    Text(" $i ")
+                                }
+                            }
+                        }
                     }
                 }
-                Divider(color = Color.Green)
             }
         }
     }
 }
-
 
 @Composable
-fun aToZhorizontal(){
-    VerticalScroller {
-        Column {
-            for (i in 'a'..'z'){
-
-                if(i == 'k'){
-                    Center{
-                        Container(width = 100.dp, height = 100.dp, alignment = Alignment.Center){ //background(Color.Green),
-                            oneToHorizontal()
+fun OneVertical(){
+    Card(color = Color.Magenta) {
+        Container(alignment = Alignment.Center, expanded = true){
+            VerticalScroller {
+                Column {
+                    for (i in 'a'..'z') {
+                        if (i=='d') {
+                            Container(height = 150.dp) {
+                                TwoHorizontal()
+                            }
+                            //Text("HERE")
+                        } else {
+                            Center(){
+                                Container(height = 100.dp){
+                                    Text("$i")
+                                }
+                            }
                         }
                     }
-
                 }
-                else{
-                    Center{
-                        Container(height = 100.dp, alignment = Alignment.Center){ //background(Color.Transparent),
-                            Text("$i")
-                        }
-                    }
-                }
-
             }
-            Divider(color = Color.Red)
         }
     }
 }
-
 
 @Composable
-fun aToZvertical(){
-    VerticalScroller {
-        Column {
-            for (i in 'a'..'z'){
-
-                if(i == 'k'){
-                    Center{
-                        Container(width = 100.dp, height = 100.dp, alignment = Alignment.Center){ //background(Color.Green),
-                            oneToHorizontal()
-                        }
-                    }
-
-                }
-                else{
-                    Center{
-                        Container(height = 100.dp, alignment = Alignment.Center){ //background(Color.Transparent),
-                            Text("$i")
-                        }
+fun TwoHorizontal(){
+    Card(color = Color.White){
+        Container(alignment = Alignment.Center, expanded = true) {
+            HorizontalScroller(isScrollable = true) {
+                Row {
+                    for (i in 1..100) {
+                        Text(" $i ")
                     }
                 }
-
             }
-            Divider(color = Color.Red)
         }
     }
 }
+
